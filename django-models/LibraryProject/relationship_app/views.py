92352204e0819afff75c2053db1cfbd5a9236b81
Timothy Ngari <Timothykabithi@gmail.com>
2025-07-27 14:56:38 +0300

    templates2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@
 
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  
+from .models import Book, Library
+from .models import Library  
 
 def list_books(request):
     books = Book.objects.all()
