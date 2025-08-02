97657f6bad60dbc2f3d9e58e071e4d423882532b
Timothy Ngari <Timothykabithi@gmail.com>
2025-08-02 12:39:58 +0300

    advanced features

---
+++ advanced_features_and_security/LibraryProject/bookshelf/url.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='list_books'),
+    path('add/', views.add_book, name='add_book'),
+    path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+]
+
+
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile
 
-from advanced_features_and_security.LibraryProject.relationship_app.models import UserProfile
+
 
 admin.site.register(UserProfile)# Register your models here.
 
