9866d3b903dad17ffc70fdd547c5591c6e5c8e44
Timothy Ngari <Timothykabithi@gmail.com>
2025-08-02 14:45:08 +0300

    advanced features

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import permission_required, login_required
 from .models import Book
+from .forms import ExampleForm
+
+from django.db.models import Q  # Import Q for complex lookups
 
 @permission_required('bookshelf.view_book', raise_exception=True)
 @login_required
 def book_list(request):
-    books=Book.objects.all()
-    return render(request, "relationship_app/book_list.html", {'books': books})
+    query = request.GET.get('q')  # Get the search term from query parameters
+    if query:
+        # Safe ORM filtering using Q objects
+        books = Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+    else:
+        books = Book.objects.all()
+        
+    return render(request, "relationship_app/book_list.html", {'books': books, 'query': query})
+
 
 @permission_required('relationship_app.can_add_book',raise_exception=True) 
 def add_book(request):
