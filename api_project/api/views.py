9a67c4ae912eba1fe03f97c7172d909a873e9b0d
Timothy Ngari <Timothykabithi@gmail.com>
2025-08-10 23:17:52 +0300

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 
+class BookList(generics.ListAPIView):
+    pass
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # Django REST Framework
     'api',
+    'rest_framework.authtoken',#token authentication
 ]
 
 MIDDLEWARE = [
