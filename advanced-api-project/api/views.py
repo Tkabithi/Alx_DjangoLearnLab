9b90747f3a86be8a9d1cf1f8e18992fec5e0a591
Timothy Ngari <Timothykabithi@gmail.com>
2025-08-17 22:48:51 +0300

    adv api project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework import generics,permissions
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+
+# ListBooksView:    GET all books
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# BookDetailView:   GET single book by ID   
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+#CreateBookView:   POST a new book
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def perform_create(self, serializer):
+        serializer.save()
+
+# UpdateBookView:   PUT/PATCH update an existing book 
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.object.all()
+    serializer_class =BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def perform_update(self, serializer):
+        serializer.save()
+
+ #   DeleteBookView:   DELETE an existing book
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
