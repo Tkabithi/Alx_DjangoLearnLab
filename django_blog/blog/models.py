2ca92d1d742b663047a2575742d6ee48061fe478
Timothy Kabithi <timothykabithi@gmail.com>
2025-09-07 14:24:07 +0300

    djangoblog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -10,4 +12,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
