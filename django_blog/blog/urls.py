273b9ac65339b99e9f588ae1e4541c2df0a33033
Timothy Kabithi <timothykabithi@gmail.com>
2025-09-07 23:16:22 +0300

    djangoblog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,10 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('register/',views.register,name='register'),
-    path('profile/',views.profile,name='profile'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('posts/', views.posts, name='posts'),  # Add this line
 ]
