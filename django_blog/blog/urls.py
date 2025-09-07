1982980b8dda7e8ed774031e7ce7ef7153b8da5c
Timothy Kabithi <timothykabithi@gmail.com>
2025-09-08 00:50:05 +0300

    djangoblog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('posts/', PostListView.as_view(), name='post-list'),
+    path('post/<int:post_id>/comments/new/', views.add_comment, name='add-comment'),
+    path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit-comment'),
+    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete-comment'),
 ]
