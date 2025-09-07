642c781980cd4d369acfd6637c8a297031da2793
Timothy Kabithi <timothykabithi@gmail.com>
2025-09-07 17:14:42 +0300

    djangoblog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            return render(request,'blog/register.html',{'form':form})
+        
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+            
